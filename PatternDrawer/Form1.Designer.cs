f1c528951165897c73e43d04cad13ea6ac27334e
taihon <qlineb@gmail.com>
2012-12-13 15:44:23 +0800

    13.12.2012

--- PatternDrawer/Form1.Designer.cs
+++ PatternDrawer/Form1.Designer.cs
@@ -28,46 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(342, 183);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 212);
+            this.button1.Location = new System.Drawing.Point(23, 440);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(74, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 5);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 480);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(437, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10000, 10000);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.panel2, 5);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(437, 431);
+            this.panel2.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(443, 517);
+            this.tableLayoutPanel1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 243);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(443, 517);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- PatternDrawer/Form1.cs
+++ PatternDrawer/Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Reflection;
 
 namespace PatternDrawer
 {
@@ -15,6 +18,85 @@ namespace PatternDrawer
         {
             InitializeComponent();
             //asdf
+            this.panel1.Paint += new PaintEventHandler(redraw_panel1);
+            
+        }
+
+        void redraw_panel1(object sender, PaintEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Pen myPen = new Pen(Color.Blue,5);
+            e.Graphics.DrawLine(myPen, new Point(0, 0), new Point(1000, 10000));
+            draw_grid(e);
+            //Figure myFigure = new Figure();
+            //myFigure.type = "coat";
+            //myFigure.setup();
+            //MessageBox.Show(myFigure.getBaseName());
+        }
+        void draw_grid(PaintEventArgs e)
+        {
+            Pen myPen = new Pen(Color.Black);
+            for (int x = 0; x <= 100; x++)
+            {
+                e.Graphics.DrawLine(myPen, new Point(x * 100, 0), new Point(x * 100, 10000));
+                e.Graphics.DrawLine(myPen, new Point(0, x * 100), new Point(10000, x * 100));
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    public class BasicPattern
+    {
+        public string bName = "base class";
+        //private Dictionary<string, Action<int>> primitiveMap = new Dictionary<string, Action<int>>();
+        //private void BasicPattern()
+        //{
+        //    //this.primitiveMap.Add("arc", 
+        //}
+        private void drawPrimitive(PaintEventArgs e,List<Point> Points)
+        {
+            //myPanel.DrawToBitmap();
+        }
+    }
+    public class SkirtPattern : BasicPattern
+    {
+        //private Dictionary<String, String> myList;
+    }
+    public class PantsPattern : BasicPattern
+    {
+    }
+    public class CoatPattern : BasicPattern
+    {
+        public string bName = "this is not a base class!";
+    }
+    public class Figure
+    {
+        public string type { get; set; }
+        public Dictionary<string, string> parameters = new Dictionary<string, string>();
+        dynamic figType { get; set; }
+        public void setup()
+        {
+            //dynamic figType;
+            switch (type)
+            {
+                case "skirt":
+                    this.figType = new SkirtPattern();
+                    break;
+                case "coat":
+                    this.figType = new CoatPattern();
+                    break;
+                case "pants":
+                    this.figType = new PantsPattern();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public string getBaseName()
+        {
+            return this.figType.bName;
         }
     }
 }
